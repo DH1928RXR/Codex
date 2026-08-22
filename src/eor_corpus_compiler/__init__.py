57ef372c1f76455d6806ad73d0cf4c025e6c34c4
DH1928RXR <danhebb@gmail.com>
2026-08-22 10:05:22 -0400

    K04: export entity resolution primitives

--- src/eor_corpus_compiler/__init__.py
+++ src/eor_corpus_compiler/__init__.py
@@ -1,7 +1,49 @@
-"""EOR Corpus Compiler C00/C01 primitives."""
+"""EOR Corpus Compiler K00-K04 primitives."""
 
-from .ir import CandidateAssertion, CorpusChunk, EntityMention, EpistemicType, EvidenceSpan, MemoryClass, ModelLineage, TemporalAnchor, TemporalPrecision
 from .build import BuildIdentity, canonical_json, content_id
+from .entity_model import (
+    EntityAlias,
+    EntityHypothesis,
+    EntityPairConstraint,
+    EntityRecord,
+    EntityRedirect,
+    EntityRegistrySnapshot,
+    EntityResolutionPolicy,
+    EntityResolutionResult,
+    EntityStatus,
+    FullRebuildRequired,
+    HypothesisDisposition,
+    MentionBinding,
+    ResolutionAction,
+    ResolutionDecision,
+    ResolutionEvidence,
+    ResolutionProposal,
+)
 from .extractor import ExtractionBackend, ExtractionCompiler, ExtractionResult
+from .ir import (
+    CandidateAssertion,
+    CorpusChunk,
+    EntityMention,
+    EpistemicType,
+    EvidenceSpan,
+    MemoryClass,
+    ModelLineage,
+    TemporalAnchor,
+    TemporalPrecision,
+)
+from .mentions import EntityMentionCompiler, MentionBucket, MentionIndex, MentionKey, MentionOccurrence
+from .resolver import EntityResolver
+from .validator import CandidateValidator, ValidationResult
 
-__all__ = ["CandidateAssertion", "CorpusChunk", "EntityMention", "EpistemicType", "EvidenceSpan", "MemoryClass", "ModelLineage", "TemporalAnchor", "TemporalPrecision", "BuildIdentity", "canonical_json", "content_id", "ExtractionBackend", "ExtractionCompiler", "ExtractionResult"]
+__all__ = [
+    "BuildIdentity", "canonical_json", "content_id",
+    "CandidateAssertion", "CorpusChunk", "EntityMention", "EpistemicType", "EvidenceSpan",
+    "MemoryClass", "ModelLineage", "TemporalAnchor", "TemporalPrecision",
+    "ExtractionBackend", "ExtractionCompiler", "ExtractionResult",
+    "CandidateValidator", "ValidationResult",
+    "EntityMentionCompiler", "MentionBucket", "MentionIndex", "MentionKey", "MentionOccurrence",
+    "EntityAlias", "EntityHypothesis", "EntityPairConstraint", "EntityRecord", "EntityRedirect",
+    "EntityRegistrySnapshot", "EntityResolutionPolicy", "EntityResolutionResult", "EntityStatus",
+    "FullRebuildRequired", "HypothesisDisposition", "MentionBinding", "ResolutionAction",
+    "ResolutionDecision", "ResolutionEvidence", "ResolutionProposal", "EntityResolver",
+]
